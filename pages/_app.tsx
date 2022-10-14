9d56ff0ceb82859b20bfe8a5d7ebb2da6041ad9c
Oliver Evans <oliverevans96@gmail.com>
2022-10-14 14:41:03 -0700

    Expose env vars to browser and use functional react-redux provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,31 +1,21 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Provider } from 'react-redux';
 import Layout from '../components/layout';
 
-import { useAppDispatch } from '../app/hooks';
-import { connect } from '@giantmachines/redux-websocket';
 import { wrapper } from '../app/store';
-import { stripVTControlCharacters } from 'util';
 
 // import { newSignup } from '../app/ws';
 
 
-function MyApp({ Component, pageProps }: AppProps) {
-  const ws_starter = start_websocket_connection();
-  ws_starter();
-  return <Layout><Component {...pageProps} /></Layout>
-}
-
-export default wrapper.withRedux(MyApp);
-
-function start_websocket_connection() {
-  return () => {
-    const url = process.env.WS_URL;
-    if (url) {
-      const dispatch = useAppDispatch()
-      dispatch(connect(url));
-    } else {
-      console.error("Could not find WS_URL");
-    }
-  }
+export default function MyApp({ Component, ...rest }: AppProps) {
+  // https://github.com/kirill-konshin/next-redux-wrapper#wrapperusewrappedstore
+  const { store, props } = wrapper.useWrappedStore(rest);
+  return (
+    <Provider store={store} >
+      <Layout>
+        <Component {...props.pageProps} />
+      </Layout>
+    </Provider>
+  );
 }
--- pages/live.tsx
+++ pages/live.tsx
@@ -2,13 +2,32 @@ import dynamic from 'next/dynamic';
 import SignupList from '../components/signup_list';
 // import Video from '../components/video';
 
+import { useAppDispatch } from '../app/hooks';
+import { connect } from '@giantmachines/redux-websocket';
+
 export default function Live() {
+  // Start WebSocket connection
+  let ws_starter = start_websocket_connection();
+  ws_starter();
+
   const Camera = dynamic(() => import('../components/camera'), { ssr: false });
   return (
     <>
-      
       <SignupList />
       {/* <Camera /> */}
     </>
   );
+}
+
+
+function start_websocket_connection() {
+  return () => {
+    const url = process.env.NEXT_PUBLIC_WS_URL;
+    if (url) {
+      const dispatch = useAppDispatch()
+      dispatch(connect(url));
+    } else {
+      console.error("Could not find WS_URL");
+    }
+  }
 }
