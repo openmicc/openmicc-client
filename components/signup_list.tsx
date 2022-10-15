98ae45ff5232352049058995bc8ff0855332f09e
Oliver Evans <oliverevans96@gmail.com>
2022-10-15 16:36:03 -0700

    Number the signup list entries

--- components/signup_list.tsx
+++ components/signup_list.tsx
@@ -23,7 +23,7 @@ export default function SignupList() {
 
     return <div className={divClasses}>
         <h2 className="m-2 font-bold text-center">The Signup List</h2>
-        <ol className="list-disc m-2 ml-4 list-outside">
+        <ol className="list-decimal m-2 ml-4 list-outside">
             {signupList.map((entry, index) => <SignupListItem key={index} entry={entry} />)}
         </ol>
         <SignupForm />
