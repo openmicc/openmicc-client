023177fa44d415837cdf9a3f1f8ed8f3e822bae1
Oliver Evans <oliverevans96@gmail.com>
2022-10-17 14:05:58 -0700

    Move client messages to new module

--- app/messages/client.ts
+++ app/messages/client.ts
@@ -1,3 +1,24 @@
 // Messages sent from client to server
 
-export {};
+import { createAction } from "@reduxjs/toolkit";
+import { IdAndReceipt, PerformParams } from "../types";
+
+/* GetList */
+
+export interface GetList {}
+export const getList = createAction<GetList>("getList");
+
+/* SignMeUp */
+
+export type SignMeUp = string;
+export const signMeUp = createAction<SignMeUp>("signMeUp");
+
+/* TakeMeOff */
+
+export type TakeMeOff = IdAndReceipt;
+export const takeMeOff = createAction<TakeMeOff>("takeMeOff");
+
+/* ImReady */
+
+export type ImReady = PerformParams;
+export const imReady = createAction<ImReady>("imReady");
