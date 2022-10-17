07514381f76a32999d0d5cfef25e532ab2662ed6
Oliver Evans <oliverevans96@gmail.com>
2022-10-17 14:06:46 -0700

    New module for custom types in API

--- app/types.ts
+++ app/types.ts
@@ -1,5 +1,20 @@
+import { RtpParameters } from "mediasoup-client/lib/RtpParameters";
+
+export type SignupListEntryText = string;
+export type SignupReceipt = string;
+export type SignupId = number;
+
 export interface SignupListEntry {
-    id: number;
-    text: string;
-  }
-  
+  id: SignupId;
+  text: SignupListEntryText;
+}
+
+export interface IdAndReceipt {
+  id: SignupId;
+  receipt: SignupReceipt;
+}
+
+export interface PerformParams {
+  signup_receipt: SignupReceipt;
+  rtp_parameters: RtpParameters;
+}
