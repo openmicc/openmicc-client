340bf0d065222e537e4fc740fa0a5f34225b3a16
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-10-17 21:18:38 -0700

    added website title to navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,6 +5,7 @@ export default function Navbar() {
       <Link href="/">
         <a className="underline text-blue-700 my-1 mx-2">Back to home</a>
       </Link>
+      <h1 className="text-black text-xl font-bold ">Openmi.cc</h1>
       <p className="my-1 mx-2 text-blue-400">navbar</p>
     </div>
   );
